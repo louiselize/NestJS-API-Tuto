ee58a1dcb93ea5dad8ca33189131d09224a6a3e9
Louise Lizé <louise.lize@icloud.com>
2024-07-10 21:12:59 +0200

    ✨ Add User and Bookmark module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, UserModule, BookmarkModule],
   controllers: [],
   providers: [],
 })
