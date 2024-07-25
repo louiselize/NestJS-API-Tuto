bd8d5d331afadf4fa04bc060b44d12d08ffea135
Louise Lizé <louise.lize@icloud.com>
2024-07-25 19:19:09 +0200

    ✅  Implement test structure

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test } from "@nestjs/testing"
 import { AppModule } from "../src/app.module"
 import { INestApplication, ValidationPipe } from "@nestjs/common"
 import { PrismaService } from "../src/prisma/prisma.service";
+import * as pactum from 'pactum'
 
 describe('App e2e', () => {
   let app: INestApplication;
@@ -19,15 +20,35 @@ describe('App e2e', () => {
       })
     )
     await app.init()
+    await app.listen(3333)
 
     prisma = app.get(PrismaService)
     await prisma.cleanDb()
+    pactum.request.setBaseUrl('http://localhost:3333')
 
   });
 
   afterAll(() => {
       app.close()
   })
+
+  describe('Auth', () => {
+    describe('Signup',() => {});
+    describe('Signin',() => {});
+  });
+
+  describe('User', () => {
+    describe('Get me',() => {});
+    describe('Edit user',() => {});
+  });
+
+  describe('Bookmarks', () => {
+    describe('Create Bookmark',() => {});
+    describe('Get Bookmarks',() => {});
+    describe('Get Bookmark by id',() => {});
+    describe('Edit Bookmarks',() => {});
+    describe('Delete Bookmark',() => {});
+  });
 
   it.todo('should pass')
 
