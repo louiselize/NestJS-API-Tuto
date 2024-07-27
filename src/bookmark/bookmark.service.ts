5b78b62edb9a861b540e8fb716ba6e9fcfb69143
Louise Lizé <louise.lize@icloud.com>
2024-07-27 16:49:15 +0200

    ✨ Get and Create implementation in Bookmark Service

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from '@nestjs/common';
 import { CreateBookmarkDto, EditBookmarkDto } from './dto';
+import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
 export class BookmarkService {
+    constructor(private prisma: PrismaService){}
 
-    getBookmarks(userId: number){}
+    getBookmarks(userId: number){
+        return this.prisma.bookmark.findMany({
+            where:{
+                userId
+            }
+        })
+    }
 
     getBookmarkById(userId: number, bookmardId: number){}
 
-    createBookmark(userId: number, dto: CreateBookmarkDto){}
+    async createBookmark(userId: number, dto: CreateBookmarkDto){
+        const bookmark = await this.prisma.bookmark.create({
+            data: {
+                userId,
+                ...dto,
+            }
+        })
+
+        return bookmark
+    }
 
     editBookmarkById(userId: number, bookmardId, dto: EditBookmarkDto){}
 
