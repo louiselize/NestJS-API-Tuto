0934c2377bbd714318fef4a1d3e9573f2ca9f829
Louise Lizé <louise.lize@icloud.com>
2024-07-27 15:04:45 +0200

    ✅ GetMe() User test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -129,12 +129,23 @@ describe('App e2e', () => {
           '/auth/signin'
         ).withBody(dto)
         .expectStatus(200)
+        .stores('userAt','access_token')
       })
     });
   });
 
   describe('User', () => {
-    describe('Get me',() => {});
+    describe('Get me',() => {
+      it('should get current user', () => {
+        return pactum
+          .spec()
+          .get('/users/me')
+          .withHeaders({
+            Authorization: 'Bearer $S{userAt}'
+          })
+          .expectStatus(200)
+      })
+    });
     describe('Edit user',() => {});
   });
 
